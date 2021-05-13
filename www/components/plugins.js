73217ff70077bb7bf1638e8ee727a73e8d213f87
david042 <david.mal04@hotmail.com>
2021-05-14 06:45:18 +0900

    add tudo

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,22 @@
-// This is a JavaScript file
+$(document).on("click","#alerta",function(){
+  navigator.notification.alert("minha mensagem",null,"Aviso!","Aceito");
+});
 
+$(document).on("click","#confirm",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == 1){
+      navigator.notification.confirm("Escolha a opção A");
+    }else{
+      navigator.notification.confirm("Escolha a opção B");
+    }
+  }
+  navigator.notification.confirm("Escolha A ou B",confirma,"Escolha: ",['A','B']);
+});
+
+$(document).on("click","#beep",function(){
+   navigator.notification.beep(3);
+});
+
+$(document).on("click","#vibrar",function(){
+   navigator.vibrate(3000);
+});
